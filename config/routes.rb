b7ee0e0303d520dbe46aa2c09cf182187d1e935b
Isaac Elias <ielias@csumb.edu>
2011-12-28 02:46:00 -0800

    Finished chapter 3 of the rails tutorial. MostlyStaticPages. Now doing TDD. Yay.

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rortut::Application.routes.draw do
   get "pages/contact"
   
   get "pages/about"
+  
+  get "pages/help"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
