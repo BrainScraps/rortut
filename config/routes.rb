ce5b090d19169b7f8cd2239f4f7f4c1e2ac25ea6
Isaac Elias <ielias@csumb.edu>
2011-12-30 02:40:06 -0800

    Added the new controller for the Users and a stub signin page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rortut::Application.routes.draw do
-  get "pages/home"
+	
+  get "users/new"
 
-  get "pages/contact"
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
+  match '/signup',  :to => 'users#new'
   
-  get "pages/about"
-  
-  get "pages/help"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -55,7 +56,7 @@ Rortut::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+root :to => 'pages#home'
 
   # See how all your routes lay out with "rake routes"
 
